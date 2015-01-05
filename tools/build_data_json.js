cce04ec810af563f58d184324651c6566ceee291
Annie Daniel <adaniel@texastribune.org>
2015-01-05 15:33:15 -0600

    added google doc stuff

---
+++ tools/build_data_json.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+var marked = require('marked');
+var XLSX = require('xlsx');
+
+marked.setOptions({
+  smartypants: true
+});
+
+var SHEETS = [
+  'META',
+  'PERRY',
+  'CLINTON'
+];
+
+var DATA = {};
+
+var workbook = XLSX.readFile('data.xlsx');
+
+SHEETS.forEach(function(sheet) {
+  'use strict';
+
+  var worksheet = workbook.Sheets[sheet];
+
+  var temp = {};
+
+  for (var cell in worksheet) {
+    if (cell[0] === '!') { continue; }
+    if (cell[0] === 'A') {
+      var aCell = worksheet[cell];
+      aCell = aCell ? aCell.v : '';
+
+      var cellNumber = cell.match(/\d+/)[0];
+
+      var bCell = worksheet['B' + cellNumber];
+      bCell = bCell ? bCell.v : '';
+
+      var cCell = worksheet['C' + cellNumber];
+      cCell = cCell ? cCell.v : '';
+
+
+      if (cCell === 'markdown') {
+        bCell = marked(bCell);
+      }
+
+      temp[aCell] = bCell;
+    }
+  }
+
+  DATA[sheet] = temp;
+});
+
+fs.writeFileSync('data.json', JSON.stringify(DATA, null, 2));
